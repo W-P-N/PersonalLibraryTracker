2175191ee75a9c4010a7b7c3d2d2cec9c6c3cc59
Parth <parthwani22@gmail.com>
2026-05-10 22:20:22 +0530

    feat: Add User entity. Add book as fk.

---
+++ src/main/java/com/wpn/personallibrarytracker/entity/User.java
@@ -0,0 +1,31 @@
+package com.wpn.personallibrarytracker.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Objects;
+
+@Entity
+@Data
+public class User {
+    @Id
+    private Integer userId;
+    private String userName;
+    private String email;
+    private String password;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private Book bookId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(userId);
+    }
+}
