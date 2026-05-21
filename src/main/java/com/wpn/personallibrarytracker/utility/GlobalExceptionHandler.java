218dfad50c376ba6c2391f1838756f8b48a1f041
Parth <parthwani22@gmail.com>
2026-05-21 20:27:04 +0530

    Feat: Update global exception handling class with exceptions of user vertical.

--- src/main/java/com/wpn/personallibrarytracker/utility/GlobalExceptionHandler.java
+++ src/main/java/com/wpn/personallibrarytracker/utility/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.wpn.personallibrarytracker.utility;
 
+import com.wpn.personallibrarytracker.exceptions.UserAlreadyExistsException;
+import com.wpn.personallibrarytracker.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,5 +16,21 @@ public class GlobalExceptionHandler {
                 HttpStatus.INTERNAL_SERVER_ERROR.value()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler({UserAlreadyExistsException.class, UserNotFoundException.class})
+    public ResponseEntity<ErrorResponse> handleUserExceptions(RuntimeException exception) {
+        ErrorResponse errorResponse = null;
+        if(exception instanceof UserAlreadyExistsException) {
+            errorResponse = new ErrorResponse(
+                    exception.getMessage(),
+                    HttpStatus.NOT_FOUND.value()
+            );
+            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
+        errorResponse = new ErrorResponse(
+                exception.getMessage(),
+                HttpStatus.CONFLICT.value()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 }
